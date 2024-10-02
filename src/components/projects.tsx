fac9099ec514d0101e8cde2714de2826db074b68
Shah Fahad <fahaddoc600@gmail.com>
2024-10-02 17:54:53 +0500

    update the projects and about us 2

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -12,6 +12,7 @@ export  default function Projects(){
             <SectionHeading>
                 My Projects
             </SectionHeading>
+
             <div>
                 {
                     projectsData.map((project, index)=>(
